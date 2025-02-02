381bd075f962e0a24cc69d7fa6594dce7f0f2b7a
malakhbiyel <malak_hbiyel@outlook.fr>
2025-02-02 11:33:16 +0100

    second commit

---
+++ src/main/java/devxplorers/heart_rate_monitor/HeartRateMonitorApplication.java
@@ -0,0 +1,13 @@
+package devxplorers.heart_rate_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartRateMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartRateMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/devxplorers/heart_rate_monitor/HeartRateMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package devxplorers.heart_rate_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartRateMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
