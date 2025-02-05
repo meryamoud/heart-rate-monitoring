8c7c0aa4e7c7dfb71ff20d38e706786fe6e7b608
maryem <maryemoudoud@gmail.com>
2025-02-05 23:50:30 +0100

    Implement Twilio SMS service functionality

---
+++ src/main/java/devxplorers/heart_rate_monitor/Controller/NotificationController.java
@@ -0,0 +1,20 @@
+package devxplorers.heart_rate_monitor.Controller;
+
+import devxplorers.heart_rate_monitor.Service.TwilioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+
+    @Autowired
+    private TwilioService twilioService;
+
+    @PostMapping("/send-sms")
+    public String sendSms(@RequestParam String phoneNumber, @RequestParam String message) {
+        twilioService.sendSms(phoneNumber, message);
+        return "SMS sent successfully!";
+    }
+}
