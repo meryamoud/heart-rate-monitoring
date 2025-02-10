5eba23ff0d261b36ba65257f8ffb77c63012ed38
malakhbiyel <malak_hbiyel@outlook.fr>
2025-02-11 00:14:09 +0100

    Solved the heart rate simulator error

--- src/main/java/devxplorers/heart_rate_monitor/Kafka/Consumer.java
+++ src/main/java/devxplorers/heart_rate_monitor/Kafka/Consumer.java
@@ -1,5 +1,6 @@
 package devxplorers.heart_rate_monitor.Kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,8 @@ import org.springframework.stereotype.Service;
 public class Consumer {
 
     @KafkaListener(topics = "heart_rate", groupId = "heart_rate_group")
-    public void consume(Integer heartRate) {
+    public void consume(ConsumerRecord<String, Integer> record) {
+        Integer heartRate = record.value();
         System.out.println("Consumed heart rate: " + heartRate);
     }
 }
