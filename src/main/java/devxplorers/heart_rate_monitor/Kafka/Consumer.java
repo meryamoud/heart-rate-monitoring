e00a84f734d2610d27eaf41a473b17c881a363c0
malakhbiyel <malak_hbiyel@outlook.fr>
2025-02-10 19:47:54 +0100

    Heart rate simulator with error

--- src/main/java/devxplorers/heart_rate_monitor/Kafka/Consumer.java
+++ src/main/java/devxplorers/heart_rate_monitor/Kafka/Consumer.java
@@ -1,10 +1,13 @@
 package devxplorers.heart_rate_monitor.Kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Consumer {
+
     @KafkaListener(topics = "heart_rate", groupId = "heart_rate_group")
-    public void consume(String message) {
-        System.out.println(" Message reçu de Kafka : " + message);
+    public void consume(Integer heartRate) {
+        System.out.println("Consumed heart rate: " + heartRate);
     }
 }
