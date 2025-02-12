bd171346e01f7ff9e0572632cb167adc6e767ad0
maryem <maryemoudoud@gmail.com>
2025-02-12 13:21:06 +0100

    Modified files and added .env changes for Twilio integration

--- src/main/java/devxplorers/heart_rate_monitor/Kafka/Consumer.java
+++ src/main/java/devxplorers/heart_rate_monitor/Kafka/Consumer.java
@@ -1,15 +1,22 @@
 package devxplorers.heart_rate_monitor.Kafka;
 
+
+import devxplorers.heart_rate_monitor.Twilio.TwilioService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer {
 
+    @Autowired
+    private TwilioService twilioService; // Injection du service Twilio
+
     @KafkaListener(topics = "heart_rate", groupId = "heart_rate_group")
     public void consume(ConsumerRecord<String, Integer> record) {
         Integer heartRate = record.value();
         System.out.println("Consumed heart rate: " + heartRate);
+
     }
 }
